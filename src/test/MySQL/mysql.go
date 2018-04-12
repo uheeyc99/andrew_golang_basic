69515472f4baab71a30d23ded4ca009bb989a7fd
Eric Gao <uheeyc@126.com>
2018-04-12 09:14:12 +0800

    1

--- src/test/MySQL/mysql.go
+++ src/test/MySQL/mysql.go
@@ -4,18 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	_"github.com/go-sql-driver/mysql"
+	"time"
 )
 
 func test(){
-	//fmt.Println("aaaq")
+	fmt.Println("a: " + time.Now().String())
 	db,err:=sql.Open("mysql","aiden:123456@tcp(10.10.3.227)/andrew?charset=utf8")
 	if err !=nil{
 		fmt.Printf("Open database error: %s\n",err)
 	}
+
+	fmt.Println("b: " + time.Now().String())
+
 	defer db.Close()
 	err=db.Ping()
+	fmt.Println("c: " + time.Now().String())
+
 	if err!=nil{
 		fmt.Println("Ping err",err)
+	}else{
+		fmt.Println("pong " + time.Now().String())
 	}
 
 
